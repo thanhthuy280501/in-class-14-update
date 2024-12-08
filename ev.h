589c1445df4f74351fbe23248338d112df4b3efb
Thuy Nguyen <156226788+thanhthuy280501@users.noreply.github.com>
2024-12-07 23:46:22 -0500

    Update ev.h

--- ev.h
+++ ev.h
@@ -6,11 +6,16 @@
 class EV : public Vehicle {
 private:
     int currCharge;
+    int range;
 public:
-    EV() : currCharge(0) {}
+    EV(string mk, string mdl, int rng) : Vehicle(mk, mdl), currCharge(0), range(rng) {}
     void fillUp(int x) override {
         currCharge += x;
         cout << "Charging with " << x << " kWh. Current charge: " << currCharge << " kWh." << endl;
+    }
+    void print() const override {
+        Vehicle::print();
+        cout << "Charge: " << currCharge << " kWh, Range: " << range << " miles" << endl;
     }
 };
 
