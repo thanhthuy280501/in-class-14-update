fa3dce2b9696ff574b508692b63eda247e71456e
Thuy Nguyen <156226788+thanhthuy280501@users.noreply.github.com>
2024-12-07 23:45:25 -0500

    Update vehicle.h

--- vehicle.h
+++ vehicle.h
@@ -5,8 +5,15 @@
 using namespace std;
 
 class Vehicle {
+protected:
+    string make;
+    string model;
 public:
+    Vehicle(string mk, string mdl) : make(mk), model(mdl) {}
     virtual void fillUp(int e) = 0; // Pure virtual
+    virtual void print() const {
+        cout << "Make: " << make << ", Model: " << model << endl;
+    }
     virtual ~Vehicle() = default;  // Virtual destructor
         cout << "Filling with " << e << " generic units of energy fuel." << endl;
     }
