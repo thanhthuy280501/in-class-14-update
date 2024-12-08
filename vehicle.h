17a4b0fbdd590c2a133304aa3a8adff44695c08b
Thuy Nguyen <156226788+thanhthuy280501@users.noreply.github.com>
2024-12-07 23:40:04 -0500

    Update vehicle.h

--- vehicle.h
+++ vehicle.h
@@ -6,7 +6,8 @@ using namespace std;
 
 class Vehicle {
 public:
-    void fillUp(int e) {  // Non-virtual initially
+    virtual void fillUp(int e) = 0; // Pure virtual
+    virtual ~Vehicle() = default;  // Virtual destructor
         cout << "Filling with " << e << " generic units of energy fuel." << endl;
     }
 };
